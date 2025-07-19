fd5ecdb248a8e6e04f30eaa45a2c5f2132a7060a
abhisheksanganid <abhishek.sangani.d@gmail.com>
2025-07-19 22:12:29 +0530

    Initial commit: Spring Boot Mail Service API with Maven, Java 17, Lombok and Log4j2

---
+++ src/main/java/com/example/mailservice/MailServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailServiceApiApplication.class, args);
+	}
+
+}
